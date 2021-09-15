22c0e4f36c78ca81f7865a4e0c11bdba5a3a897d
pjoao266 <pjoao266@gmail.com>
2021-09-14 23:38:55 -0300

    Atualização da base. Data: 2021-09-14 23:38

---
+++ src/atualiza_git.R
@@ -0,0 +1,47 @@
+git2r::config(user.name = "pjoao266",user.email = "pjoao266@gmail.com")
+# Git status.
+gitstatus <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git status"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+# Git add.
+gitadd <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git add --all"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+# Git commit.
+gitcommit <- function(msg = "commit from Rstudio", dir = getwd()){
+  cmd = sprintf("git commit -m\"%s\"",msg)
+  system(cmd)
+}
+
+# Git push.
+gitpush <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git push"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+gitstatus()
+gitadd()
+hoje = Sys.time() %>% 
+  str_sub(1,-4)
+gitcommit(msg = paste0('Atualização da base. Data: ',hoje))
+
+gitpush()
--- src/pega_bases.R
+++ src/pega_bases.R
@@ -12,7 +12,7 @@ dias_com_jogo <- page %>%
 datas_dias = dias_com_jogo %>% 
   html_attr('data-ts')
 
-jogos = read.table('base_tabelaDeJogos.txt') %>% 
+jogos = read.table('data/base_tabelaDeJogos.txt') %>% 
   filter(!(Dia %in% datas_dias))
 
 for (indicedia in 1:length(dias_com_jogo)) {
@@ -41,59 +41,6 @@ for (indicedia in 1:length(dias_com_jogo)) {
                                  GolsMandante==GolsVisitante~'E'))
   jogos = rbind(jogos,base_jogos_Dia)
 }
-write.table(jogos,'base_tabelaDeJogos.txt')
 
-hoje = Sys.time() %>% 
-  str_sub(1,-4)
-git2r::config(user.name = "pjoao266",user.email = "pjoao266@gmail.com")
-
-gitcommit <- function(msg = "commit from Rstudio", dir = getwd()){
-  cmd = sprintf("git commit -m\"%s\"",msg)
-  system(cmd)
-}
-# Git status.
-gitstatus <- function(dir = getwd()){
-  cmd_list <- list(
-    cmd1 = tolower(substr(dir,1,2)),
-    cmd2 = paste("cd",dir),
-    cmd3 = "git status"
-  )
-  cmd <- paste(unlist(cmd_list),collapse = " & ")
-  shell(cmd)
-}
-
-# Git add.
-gitadd <- function(dir = getwd()){
-  cmd_list <- list(
-    cmd1 = tolower(substr(dir,1,2)),
-    cmd2 = paste("cd",dir),
-    cmd3 = "git add --all"
-  )
-  cmd <- paste(unlist(cmd_list),collapse = " & ")
-  shell(cmd)
-}
-
-# Git commit.
-gitcommit <- function(msg = "commit from Rstudio", dir = getwd()){
-  cmd_list <- list(
-    cmd1 = tolower(substr(dir,1,2)),
-    cmd2 = paste("cd",dir),
-    cmd3 = paste0("git commit -am ","'",msg,"'")
-  )
-  cmd <- paste(unlist(cmd_list),collapse = " & ")
-  shell(cmd)
-}
-
-# Git push.
-gitpush <- function(dir = getwd()){
-  cmd_list <- list(
-    cmd1 = tolower(substr(dir,1,2)),
-    cmd2 = paste("cd",dir),
-    cmd3 = "git push"
-  )
-  cmd <- paste(unlist(cmd_list),collapse = " & ")
-  shell(cmd)
-}
-gitadd()
-gitcommit(msg = paste0('Atualização da base. Data: ',hoje))
-gitpush()
+write.table(jogos,'data/base_tabelaDeJogos.txt')
+source('src/atualiza_git.R')
