b46018544779dfa218c29b737697d7467df20327
pjoao266 <pjoao266@gmail.com>
2021-09-15 00:20:04 -0300

    Atualização da base. Data: 2021-09-15 00:20

--- atualiza_git.R
+++ atualiza_git.R
@@ -1,4 +1,8 @@
-git2r::config(user.name = "pjoao266",user.email = "pjoao266@gmail.com")
+library(git2r)
+dir = 'C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol'
+repo <- repository(discover_repository(dir))
+git2r::config(repo = repo,
+              user.name = "pjoao266",user.email = "pjoao266@gmail.com")
 # Git status.
 gitstatus <- function(dir = getwd()){
   cmd_list <- list(
@@ -26,7 +30,15 @@ gitcommit <- function(msg = "commit from Rstudio", dir = getwd()){
   cmd = sprintf("git commit -m\"%s\"",msg)
   system(cmd)
 }
-
+gitcommit <- function(msg = "commit from Rstudio", dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = sprintf("git commit -m\"%s\"",msg)
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
 # Git push.
 gitpush <- function(dir = getwd()){
   cmd_list <- list(
@@ -38,10 +50,10 @@ gitpush <- function(dir = getwd()){
   shell(cmd)
 }
 
-gitstatus()
-gitadd()
+gitstatus(dir)
+gitadd(dir)
 hoje = Sys.time() %>% 
   str_sub(1,-4)
-gitcommit(msg = paste0('Atualização da base. Data: ',hoje))
+gitcommit(dir = dir,msg = paste0('Atualização da base. Data: ',hoje))
 
-gitpush()
+gitpush(dir)
