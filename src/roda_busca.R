40b8674d84f1e1b237e3123c660bca9be57f22b8
pjoao266 <pjoao266@gmail.com>
2021-09-15 21:06:51 -0300

    Atualização da base. Data: 2021-09-15 21:06

--- src/roda_busca.R
+++ src/roda_busca.R
@@ -1,34 +1,36 @@
-library(RSelenium)
-library(rvest)
-library(tidyverse)
-library(git2r)
-rm(list=ls())
-ini = Sys.time()
-source('C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\src\\atualiza_git.R')
-source('C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\src\\pega_bases.R')
-source('C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\src\\acessa_pagina.R')
-
-link = 'https://www.uol.com.br/esporte/futebol/central-de-jogos/#/'
-
-
-page = acessa_pagina(link)
-
-
-jogos = pega_bases(page)
-write.table(jogos,'C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\data\\base_tabelaDeJogos.txt')
-
-
-dir = 'C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol'
-repo <- repository(discover_repository(dir))
-git2r::config(repo = repo,
-              user.name = "pjoao266",user.email = "pjoao266@gmail.com")
-gitstatus(dir)
-gitadd(dir)
-hoje = Sys.time() %>% 
-  str_sub(1,-4)
-gitcommit(dir = dir,msg = paste0('Atualização da base. Data: ',hoje))
-gitpush(dir)
-
-
-fim = Sys.time()
-fim-ini
+roda_busca = function(){
+  library(RSelenium)
+  library(rvest)
+  library(tidyverse)
+  library(git2r)
+  rm(list=ls())
+  ini = Sys.time()
+  source('C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\src\\atualiza_git.R')
+  source('C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\src\\pega_bases.R')
+  source('C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\src\\acessa_pagina.R')
+  
+  link = 'https://www.uol.com.br/esporte/futebol/central-de-jogos/#/'
+  
+  
+  page = acessa_pagina(link)
+  
+  
+  jogos = pega_bases(page)
+  write.table(jogos,'C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\data\\base_tabelaDeJogos.txt')
+  
+  
+  dir = 'C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol'
+  repo <- repository(discover_repository(dir))
+  git2r::config(repo = repo,
+                user.name = "pjoao266",user.email = "pjoao266@gmail.com")
+  gitstatus(dir)
+  gitadd(dir)
+  hoje = Sys.time() %>% 
+    str_sub(1,-4)
+  gitcommit(dir = dir,msg = paste0('Atualização da base. Data: ',hoje))
+  gitpush(dir)
+  
+  
+  fim = Sys.time()
+  fim-ini
+}
