5bf070b9687f65e2f610e99377fd2788e2d5b422
pjoao266 <pjoao266@gmail.com>
2021-09-17 13:43:40 -0300

    Atualização da base. Data: 2021-09-17 13:43

--- src/acessa_pagina.R
+++ src/acessa_pagina.R
@@ -1,8 +1,7 @@
 acessa_pagina = function(link){
   rd = rsDriver(browser = 'chrome',port=1237L,
                 chromever = '93.0.4577.63',
-                geckover = '0.29.0',
-                extraCapabilities = list("chromeOptions" = list(args = list('--headless'))))
+                geckover = '0.29.0')
   cliente = rd$client
   servidor = rd$server
   cliente$navigate(link)
