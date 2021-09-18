a8782c7043de6b545868472a3a1fffaa3f8d03c2
pjoao266 <pjoao266@gmail.com>
2021-09-18 12:04:43 -0300

    Atualização da base. Data: 2021-09-18 12:04

--- src/roda_busca.R
+++ src/roda_busca.R
@@ -18,7 +18,7 @@ roda_busca = function(){
   
   jogos = pega_bases(page)
   write.table(jogos,'C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\data\\base_tabelaDeJogos.txt')
-  
+  saveRDS(jogos,'C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol\\data\\base_tabelaDeJogos.RDS')
   dir = 'C:\\Users\\JoaoPedro\\Arquivos\\Dados\\Maluquices\\JogosFutebol'
   repo <- repository(discover_repository(dir))
   git2r::config(repo = repo,
